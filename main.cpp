b87efb71fe9e21c72f76c67cf70204e91f02111a
Patrick Mutwiri <patwiri@gmail.com>
2020-03-12 14:04:18 +0300

    q3

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Voter {
+    int voter_id, id_number;
+    char gender[1];
+    string fname, mname, lname, surname, polling_station, dob;
+
+    void registerVoter(){
+
+    }
+
+    void showVoterDetails(){
+
+    }
+};
+
+int main()
+{
+    cout << "\n\n
+    cout << "\n\n :: EVMS ::";
+    cout << "\n\n
+
+    return 0;
+}
